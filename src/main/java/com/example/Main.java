3fc9d0ee21471a36c115df591941ca064007b803
sumingchun <su.mingchun@f.flect.co.jp>
2020-02-17 14:20:26 +0900

    Demo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -79,7 +79,7 @@ public class Main {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getTimestamp("name"));
       }
 
       model.put("records", output);
