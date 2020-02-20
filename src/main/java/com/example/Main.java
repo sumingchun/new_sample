837768781d65c09c90ed6486e6364b3aa91da01a
sumingchun <su.mingchun@f.flect.co.jp>
2020-02-20 11:10:43 +0900

    Demo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -91,6 +91,28 @@ public class Main {
     }
   }
 
+  @RequestMapping("/staff")
+  String staff(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.execute("set search_path=salesforce, public;");
+      ResultSet rs = stmt.executeQuery("SELECT name FROM staff__c");
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Read from Staff: " + rs.getString("name"));
+      }
+
+      model.put("records", output);
+      return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
   @Bean
   public DataSource dataSource() throws SQLException {
     if (dbUrl == null || dbUrl.isEmpty()) {
