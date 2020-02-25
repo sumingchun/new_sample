9f9757daf9a27210222692c9132f329d25be101e
“sumingchun” <su.mingchun@f.flect.co.jp>
2020-02-25 14:46:16 +0900

    Demo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -29,9 +29,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-
-import java.util.HashMap;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
---
+++ src/main/java/com/example/Staff.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Staff {
+  private Long id;
+
+  @NotBlank(message="職員の名称を記入してください。")
+  private String name;
+
+  @Min(value=1, message="1以上の数値を入力してください。")
+  @Max(value=4, message="4以下の数値を入力してください。")
+  private String staffId;
+
+  @Size(max=3, message="年齢は3桁数字を超えないでください。")
+  private String age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getStaffId() {
+    return staffId;
+  }
+
+  public void setStaffId(String staffId) {
+    this.staffId = staffId;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
+}
