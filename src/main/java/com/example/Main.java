47d80a527ee0632467eaa81b84f00bb4f675ccb1
“sumingchun” <su.mingchun@f.flect.co.jp>
2020-02-26 19:05:15 +0900

    Demo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -40,10 +40,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
